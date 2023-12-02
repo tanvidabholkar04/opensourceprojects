82fb364ea718e44c1cd3767c0e14c9726cfb1b80
tanvidabholkar04 <141729517+tanvidabholkar04@users.noreply.github.com>
2023-12-02 08:40:20 +0530

    Update demo.py
    changes done by branch1

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
 print("This is first line of main branch".)
-print("this is project is under open source projects.")
+print("This is project is under open source projects.")
 print("Sky has beautiful clouds today.")
+print("This is first line of branch1.")
+print("Today's date is 2nd December 2023.")
